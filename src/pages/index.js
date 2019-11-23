b5b8464d7b4910f706a8b3f76dca43ce3b319471
Gilad Grant <ggrant966@gmail.com>
2019-11-23 01:28:50 +0000

    Change mobile holding page styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,25 +4,23 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-import logo from '../images/faw_logo.svg'
-import instagramLogo from '../images/instagram.svg'
+import { Logo, Instagram } from '../components/icons'
 
 const IndexPage = () => {
     const categories = ['Recipes', 'Culture', 'Reviews', 'Blog'];
-    const images = [{ title: 'instagram', src: instagramLogo, link: 'https://www.instagram.com/foodandwriting/' }];
 
     return (
         <Layout>
             <SEO title="Home" description="Website coming soon" />
             <section>
                 <div className="caption">
-                    <img className="logo" src={logo} alt="Food and Writing logo" />
+                    <Logo />
 
                     <hr />
 
                     <h2>Website coming soon...</h2>
 
-                    <p className="bio">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae.</p>
+                    <p className="bio">Making food look pretty on an old bathroom tile.</p>
 
                     <ul className="category-list">
                         {categories.map((item, index) =>
@@ -31,11 +29,9 @@ const IndexPage = () => {
                     </ul>
 
                     <ul className="icon-list">
-                        {images.map(item =>
-                            <a key={`${item.title}`} href={item.link} rel="noopener noreferrer" target="_blank">
-                                <img className={`icon ${item.title}`} src={item.src} alt={`${item.title} logo`} />
-                            </a>
-                        )}
+                        <a href="https://www.instagram.com/foodandwriting/" rel="noopener noreferrer" target="_blank">
+                            <Instagram />
+                        </a>
                     </ul>
                 </div>
             </section>

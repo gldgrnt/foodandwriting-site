05036116dca57779f1e8240631fc8f3dcfeb8420
Gilad Grant <ggrant966@gmail.com>
2019-12-14 16:45:13 +0000

    Add initial ingredient fetch

--- studio/components/shoppingListItem.js
+++ studio/components/shoppingListItem.js
@@ -4,6 +4,10 @@ import FormField from 'part:@sanity/components/formfields/default'
 
 import PatchEvent, { set, unset } from 'part:@sanity/form-builder/patch-event'
 
+//Nutrition API details
+const nut_id = process.env.SANITY_STUDIO_NUTRITIONIX_APP_ID;
+const nut_key = process.env.SANITY_STUDIO_NUTRITIONIX_API_KEY;
+
 //Tell sanity that the value has been updated
 const createPatchFrom = value => PatchEvent.from(value === '' ? unset() : set(String(value)))
 
@@ -35,7 +39,27 @@ class ShoppingListItem extends Component {
         //Update the value on sanity
         this.setState({ userValue: event.target.value })
         //this.props.onChange(createPatchFrom(event.target.value))
+        const url = `https://trackapi.nutritionix.com/v2/search/instant?query=${event.target.value}`;
+        fetch(url, {
+            method: 'GET',
+            // mode: 'no-cors',
+            headers: {
+                'x-app-id': nut_id,
+                'x-app-key': nut_key,
+            }
+        })
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+                console.log(data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }
+
+
 
 
     render() {

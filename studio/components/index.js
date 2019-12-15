ed17a37f8373896ed66d43533e1de844d1112bee
Gilad Grant <ggrant966@gmail.com>
2019-12-15 21:53:58 +0000

    Add shopping list item search

--- studio/components/index.js
+++ studio/components/index.js
@@ -1,3 +1,3 @@
 export { default as FlexImages } from './FlexImages'
 export { default as TimeSelect } from './TimeSelect'
-export { default as ShoppingListItem } from './shoppingListItem'
+export { default as ShoppingListItem } from './ShoppingListItem'

2283bcd2156ed2a08faad6b7c8d192d3b6df1b5b
Gilad Grant <ggrant966@gmail.com>
2020-02-01 22:52:41 +0000

    Add section component

---
+++ web/src/components/layout/Section.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+import { responsiveBreakpointDown } from '../../utils'
+import { themeVariables } from '../../styles'
+
+export const Section = ({ children, spacingTop = "0", spacingBottom = "0" }) => {
+
+    return (
+        <StyledSection
+            spacingTop={makeStyles(spacingTop, 'padding-top')}
+            spacingBottom={makeStyles(spacingBottom, 'padding-bottom')}>
+            {children}
+        </StyledSection>
+    )
+}
+
+Section.prototypes = {
+    children: PropTypes.node.isRequired,
+    spacingTop: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.object
+    ]),
+    spacingBottom: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.object
+    ])
+}
+
+const StyledSection = styled.section`
+    ${props => props.spacingTop}
+    ${props => props.spacingBottom}
+`
+
+// MAKE THIS RESPONSIVE
+const makeStyles = (value, styleName) => {
+    let styles = ''
+    const spacing = themeVariables.grid.spacing
+
+    switch (typeof value) {
+
+        case 'string': {
+            styles += `${styleName}: ${parseInt(value) * spacing}px;`
+            break
+        }
+
+        case 'object': {
+            for (const breakpointName in value) {
+                styles += responsiveBreakpointDown(breakpointName,
+                    `${styleName}: ${parseInt(value[breakpointName]) * spacing}px;`
+                )
+            }
+            break
+        }
+
+        default:
+            return
+    }
+
+    return styles
+}
--- web/src/components/layout/index.js
+++ web/src/components/layout/index.js
@@ -2,5 +2,6 @@ import { Page } from './Page'
 import { GridContainer } from './GridContainer'
 import { GridRow } from './GridRow'
 import { GridCol } from './GridCol'
+import { Section } from './Section'
 
-export { Page, GridContainer, GridRow, GridCol }
+export { Page, GridContainer, GridRow, GridCol, Section }

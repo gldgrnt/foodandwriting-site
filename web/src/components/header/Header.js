159892c2146d0f709f181feb79e43052783bca60
Gilad Grant <ggrant966@gmail.com>
2020-02-09 15:41:14 +0000

    Reduce padding

--- web/src/components/header/Header.js
+++ web/src/components/header/Header.js
@@ -5,48 +5,48 @@ import { Navigation, Logo, Toggle, SearchContainer } from './components'
 import { GridContainer } from '../layout'
 
 export const Header = () => {
-    const [dropdownState, setDropdownState] = useState({ isSearchOpen: false, isMenuOpen: false });
-    const [dropdownInnerHeight, setDropdownInnerHeight] = useState(0);
+    const [dropdownState, setDropdownState] = useState({ isSearchOpen: false, isMenuOpen: false })
+    const [dropdownInnerHeight, setDropdownInnerHeight] = useState(0)
 
     const handleToggleClick = (toggling = null) => {
         if (toggling === null) {
-            return;
+            return
         }
-        let closing = toggling === 'isSearchOpen' ? 'isMenuOpen' : 'isSearchOpen';
+        let closing = toggling === 'isSearchOpen' ? 'isMenuOpen' : 'isSearchOpen'
         // Close menu if it's already open
         if (dropdownState[toggling]) setDropdownState({ [toggling]: false });
         // Open menu
-        else setDropdownState({ [toggling]: true, [closing]: false });
+        else setDropdownState({ [toggling]: true, [closing]: false })
     }
 
     // Set up click elsewhere event handler - call once via []
     useEffect(() => {
         document.querySelectorAll('main, footer').forEach(area => {
             area.addEventListener('click', () => {
-                setDropdownState({ isSearchOpen: false, isMenuOpen: false });
+                setDropdownState({ isSearchOpen: false, isMenuOpen: false })
             })
-        });
+        })
     }, [])
 
     // Create ref to dropdown container to calculare child height after render
-    const dropdownInner = useRef(null);
+    const dropdownInner = useRef(null)
     useEffect(() => {
         if (dropdownState.isSearchOpen === true || dropdownState.isMenuOpen === true) {
-            setDropdownInnerHeight(dropdownInner.current.clientHeight);
+            setDropdownInnerHeight(dropdownInner.current.clientHeight)
         }
     }, [dropdownState])
 
     // Conditionally render dropdowns
-    const isDropdownOpen = dropdownState.isSearchOpen || dropdownState.isMenuOpen;
-    let activeDropdown = null;
+    const isDropdownOpen = dropdownState.isSearchOpen || dropdownState.isMenuOpen
+    let activeDropdown = null
 
     // Pass close menu func down as prop
     const closeDropdown = () => {
-        setDropdownState({ isSearchOpen: false, isMenuOpen: false });
+        setDropdownState({ isSearchOpen: false, isMenuOpen: false })
     }
 
-    if (dropdownState.isSearchOpen) activeDropdown = <SearchContainer closeDropdown={closeDropdown} />;
-    else if (dropdownState.isMenuOpen) activeDropdown = <span>Menu dropdown</span>;
+    if (dropdownState.isSearchOpen) activeDropdown = <SearchContainer closeDropdown={closeDropdown} />
+    else if (dropdownState.isMenuOpen) activeDropdown = <span>Menu dropdown</span>
 
 
 
@@ -94,7 +94,7 @@ const StyledHeader = styled.header`
 `
 
 const ItemWrapper = styled.div`
-    padding: 35px 0;
+    padding: 30px 0;
     flex-grow: ${props => props.grow ? "1" : "0"};
 `
 

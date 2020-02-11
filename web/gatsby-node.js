d4d4ef49c077e73ea5943dafb380c9d95b129a38
Gilad Grant <ggrant966@gmail.com>
2020-02-11 07:17:40 +0000

    Programmatically add pages to site - progress commit

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -1,7 +1,44 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+// Create pages posts
+exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions
 
-// You can delete this file if you're not using it
+    const pagesToCreate = await graphql(`
+        {
+            allSanityRecipe {
+                edges {
+                    node {
+                        slug {
+                            current
+                        }
+                        title
+                        seoDecsription
+                        recipeIntro
+                        category {
+                            slug {
+                                current
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
+    // Check for errors
+    if (pagesToCreate.errors) {
+        throw pagesToCreate.errors
+    }
+
+    const pages = pagesToCreate.data.allSanityRecipe.edges || []
+
+    pages.forEach((edge, index) => {
+        const path = `/${edge.node.category.slug.current}/${edge.node.slug.current}`
+
+        createPage({
+            path,
+            component: require.resolve('./src/page-templates/page.js'),
+            context: {data: edge.node},
+          })
+    })
+
+}
---
+++ web/src/page-templates/page.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { Page } from '../components/layout'
+// import { SEO } from '../utils'
+
+const page = () => (
+    <Page>
+        <h1>This is the page</h1>
+    </Page>
+)
+
+export default page

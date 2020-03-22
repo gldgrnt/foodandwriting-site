083ae62feccf0a216a81f43a6fd67f4eab452199
Gilad Grant <ggrant966@gmail.com>
2020-03-22 18:25:27 +0000

    Add JSDoc comments

--- web/src/utils/post.js
+++ web/src/utils/post.js
@@ -1,13 +1,22 @@
 import moment from 'moment'
 
-export const getPostSlug = (postMeta) => {
-    const postLink = postMeta.slug.current
-    const categoryLink = postMeta.category.slug.current
-
-    return `/${categoryLink}/${postLink}`
+/**
+ * Generate post slug from post object
+ * 
+ * @param {{slug:{current: string}, category: {slug: {current: string}}}} post Post
+ * @returns {string} Post slug based on category
+ */
+export const getPostSlug = ({slug, category}) => {
+    return `/${category.slug.current}/${slug.current}`
 }
 
-
+/**
+ * Get a post's date
+ * 
+ * @param {string} date Date
+ * @param {string} [format] Date format
+ * @returns {{raw: string, formatted: string}} Raw and formatted date object
+ */
 export const getPostDate = (date, format = 'DD/MM/YYYY') => {
     return date && {
         'raw': new Date(date),

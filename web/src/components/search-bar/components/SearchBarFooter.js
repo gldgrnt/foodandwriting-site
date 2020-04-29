266c80eb4bf1b1a4098a55c3707029cf33f26fbb
Gilad Grant <ggrant966@gmail.com>
2020-04-29 09:47:14 +0100

    Add algolia logo to search bar

--- web/src/components/search-bar/components/SearchBarFooter.js
+++ web/src/components/search-bar/components/SearchBarFooter.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { FiX } from 'react-icons/fi'
+import { PoweredBy } from 'react-instantsearch-dom'
 
 import { Button } from '../../ui'
 
@@ -12,6 +13,7 @@ export const SearchBarFooter = ({ closeDropdown }) => {
     return (
         <SearchBarFooterWrapper>
             <Button secondary onClick={closeDropdown}><FiX/> Close search</Button>
+            <StyledPoweredBy translations={{ searchBy: 'Powered by'}} />
         </SearchBarFooterWrapper>
     )
 }
@@ -27,6 +29,8 @@ SearchBarFooter.propTypes = {
  * Styles
  */
 const SearchBarFooterWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
     padding-top: 20px;
 
     button {
@@ -36,6 +40,35 @@ const SearchBarFooterWrapper = styled.div`
         svg {
             stroke-width: 3px;
             margin-right: 3px;
+        }
+    }
+`
+
+const StyledPoweredBy = styled(PoweredBy)`
+    display: flex;
+    align-items: center;
+
+    span {
+        display: inline-block;
+        font-family: ${props => props.theme.font.family.sans};
+        text-transform: uppercase;
+        font-weight: 600;
+        font-size: ${props => props.theme.font.size.tiny};
+        margin-right: 5px;
+        color: ${props => props.theme.color.darkGrey};
+    }
+
+    a {
+        display: inline-flex;
+
+        svg {
+            width: 70px;
+            height: auto;
+
+            path {
+                &:first-of-type { fill: #bbb !important;}
+                &:last-of-type { fill: #555 !important;}
+            }
         }
     }
 `

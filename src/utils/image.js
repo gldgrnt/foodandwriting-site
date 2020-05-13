bbd1ba73fced011b157d16d3ba811615d4486453
Gilad Grant <ggrant966@gmail.com>
2020-05-13 14:55:49 +0100

    Add getFluidPropsFromFeaturedImage function

--- src/utils/image.js
+++ src/utils/image.js
@@ -1,7 +1,14 @@
+import _ from 'lodash'
 import { getFluidGatsbyImage } from 'gatsby-source-sanity'
 
-const sanityConfig = {projectId: process.env.GATSBY_SANITY_PROJECT_ID, dataset: process.env.GATSBY_SANITY_DATASET,}
+const sanityConfig = { projectId: process.env.GATSBY_SANITY_PROJECT_ID, dataset: process.env.GATSBY_SANITY_DATASET, }
 
+/**
+ * Get gatsby fluid image props from sanity image ID
+ * 
+ * @param {String} imageAssetId 
+ * @returns {Object} Fluid image props
+ */
 export const getFluidPropsFromId = (imageAssetId = '') => {
     try {
         if (imageAssetId.length < 1) {
@@ -10,7 +17,27 @@ export const getFluidPropsFromId = (imageAssetId = '') => {
 
         return getFluidGatsbyImage(imageAssetId, {}, sanityConfig)
 
-    } catch(err) {
+    } catch (err) {
+        return err
+    }
+}
+
+
+/**
+ * Get gatsby fluid image props from gatsby featuredImage field
+ * 
+ * @param {Object} featuredImage Gatsby featured image field
+ * @returns {Object} Fluid image props
+ */
+export const getFluidPropsFromFeaturedImage = (featuredImage) => {
+    try {
+        return !_.isNull(featuredImage) && _.hasIn(featuredImage, 'asset')
+            ? (_.hasIn(featuredImage, 'asset.fluid')
+                ? featuredImage.asset.fluid
+                : getFluidPropsFromId(featuredImage.asset._ref))
+            : false
+
+    } catch (err) {
         return err
     }
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
 import { SEO } from './SEO'
 import { responsiveBreakpointDown, responsiveStylesFromProp } from './responsive'
 import { getPostSlug, getPostDate, parseReadyInString } from './post'
-import { getFluidPropsFromId } from './image'
+import { getFluidPropsFromId, getFluidPropsFromFeaturedImage } from './image'
 
-export { SEO, responsiveBreakpointDown, getPostSlug, getPostDate, parseReadyInString, responsiveStylesFromProp, getFluidPropsFromId }
+export { SEO, responsiveBreakpointDown, getPostSlug, getPostDate, parseReadyInString, responsiveStylesFromProp, getFluidPropsFromId, getFluidPropsFromFeaturedImage }

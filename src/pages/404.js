38678dfc4894af37ab57b494f00766245edacd2a
Gilad Grant <ggrant966@gmail.com>
2020-06-10 23:27:13 +0100

    Add content to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,89 @@
 import React from "react"
-import { Page } from '../components/layout'
+import { graphql } from 'gatsby'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+
+import { PostArchive } from '../components/page-sections'
+import { Page, Section, GridContainer, GridRow, GridCol } from '../components/layout'
+import { SmallCaps, InternalLink } from '../components/ui'
 import { SEO } from '../utils'
 
-const NotFoundPage = () => (
+const FourOhFourPage = ({ data: { recentRecipes, recipesCategory } }) => (
     <Page>
-        <SEO title="404: Not found" />
-        <h1>NOT FOUND</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Page>
+        <SEO title="Page not found" />
+        <Section spacingTop="2" spacingBottom="3" >
+            <GridContainer>
+                <GridRow>
+                    <GridCol>
+                        <h1>Page not found</h1>
+                        <p>There were no results for that page.</p>
+                        <InternalLink primary to="/">Back home</InternalLink>
+                    </GridCol>
+                </GridRow>
+            </GridContainer>
+        </Section>
+
+        <Section spacingBottom="4">
+            <RecentRecipesWrapper>
+                <GridContainer justify="flex-start" align="center">
+                    <SmallCaps as="h2" color="black">Recent recipes</SmallCaps>
+                </GridContainer>
+                <PostArchive posts={recentRecipes.nodes} categoryType="Recipe" />
+                <GridContainer justify="flex-start" align="center">
+                    <InternalLink secondary to={`/${recipesCategory.slug.current}`}>View all recipes</InternalLink>
+                </GridContainer>
+            </RecentRecipesWrapper>
+        </Section>
+    </Page >
 )
 
-export default NotFoundPage
+export default FourOhFourPage
+
+/**
+ * PropTypes
+ */
+FourOhFourPage.propTypes = {
+    data: PropTypes.shape({
+        recentRecipes: PropTypes.shape({
+            nodes: PropTypes.array.isRequired
+        }).isRequired,
+        recipesCategory: PropTypes.shape({
+            slug: PropTypes.shape({
+                current: PropTypes.string.isRequired
+            }).isRequired
+        }).isRequired
+    }).isRequired
+}
+
+
+/**
+ * Styles
+ */
+const RecentRecipesWrapper = styled.div`
+    > div > h2 {
+        margin: 0;
+    }
+
+    > *:not(:last-child) {
+        margin-bottom: 40px;
+    }
+`
+
+/**
+ * GraphQL query
+ */
+export const query = graphql`
+   query FourOhFourPageQuery {
+        recentRecipes:  allSanityPost(limit: 3, sort: {order: DESC, fields: date}, filter: {category: {categoryType: {eq: "Recipe"}}}) {
+            nodes {
+                ...PreviewPostFragment
+                ...MediumFluidImageFragment
+            }
+        }
+        recipesCategory: sanityCategory(categoryType: {eq: "Recipe"}) {
+            slug {
+                current
+            }
+        }
+    } 
+`

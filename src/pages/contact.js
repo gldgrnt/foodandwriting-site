14ffdcd0c3d071b7248b770c458e2a37de25f18b
Gilad Grant <ggrant966@gmail.com>
2020-06-10 23:37:57 +0100

    Add inital contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,24 @@
+import React from 'react'
+// import { graphql } from 'gatsby'
+// import PropTypes from 'prop-types'
+
+import { SEO } from '../utils'
+import { Page } from '../components/layout'
+// import { FawBlockContent } from '../components/block-content'
+
+/**
+ * ContactPage component
+ */
+const ContactPage = () => {
+
+    return (
+        <>
+            <SEO title="Contact" />
+            <Page>
+                <h1>This is the title</h1>
+            </Page>
+        </>
+    )
+}
+
+export default ContactPage

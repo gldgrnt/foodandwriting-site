f3f122e5077e8a89365ba8e135d48e714c484eaa
Gilad Grant <ggrant966@gmail.com>
2020-06-11 17:06:38 +0100

    Add placeholder contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import styled from 'styled-components'
 // import { graphql } from 'gatsby'
 // import PropTypes from 'prop-types'
 
 import { SEO } from '../utils'
-import { Page } from '../components/layout'
+import { Page, Section, GridContainer } from '../components/layout'
 // import { FawBlockContent } from '../components/block-content'
 
 /**
@@ -15,10 +16,22 @@ const ContactPage = () => {
         <>
             <SEO title="Contact" />
             <Page>
-                <h1>This is the title</h1>
+                <Section spacingTop="2" spacingBottom="3">
+                    <GridContainer>
+                        <Title>Contact</Title>
+                    </GridContainer>
+                </Section>
+
+                <GridContainer>
+                    <p>Contact page content</p>
+                </GridContainer>
             </Page>
         </>
     )
 }
 
 export default ContactPage
+
+const Title = styled.h1`
+    margin: 0;
+`

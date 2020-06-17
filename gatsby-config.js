627bad873b0e4d6b9688bd9b5b63e053c1b27720
Gilad Grant <ggrant966@gmail.com>
2020-06-17 23:27:52 +0100

    Remove live updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,8 +32,6 @@ module.exports = {
             options: {
                 projectId: process.env.GATSBY_SANITY_PROJECT_ID,
                 dataset: process.env.GATSBY_SANITY_DATASET,
-                token: process.env.GATSBY_SANITY_READ_TOKEN,
-                watchMode: process.env.NODE_ENV !== 'production'
             },
         },
         {
